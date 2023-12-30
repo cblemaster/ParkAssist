efcf54715dfaccf89623ef8a0cc6077f66e369be
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-30 18:25:58 -0500

    added guard clauses to core project

---
+++ ParkAssist.Core/GuardClauses.cs
@@ -0,0 +1,26 @@
+﻿namespace ParkAssist.Core
+{
+    public class GuardClauses
+    {
+        public bool IsNotNull<T>(T obj) where T : class
+        {
+            return obj != null;
+        }
+        public bool StringContainsChars(string str)
+        {
+            return !string.IsNullOrWhiteSpace(str);
+        }
+        public bool StringLengthInValidRangeInclusive(int min, int max, string str)
+        {
+            return str.Length >= min && str.Length <= max;
+        }
+        public bool IdGreaterThanZero(int id)
+        {
+            return id > 0;
+        }
+        public bool DecimalGreaterThanZero(decimal dec)
+        {
+            return dec > 0M;
+        }
+    }
+}
