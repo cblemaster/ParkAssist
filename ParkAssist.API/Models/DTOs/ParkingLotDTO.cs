3fe494641c9d43f1d585cd474f6e83e0d5a123d2
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-30 22:22:43 -0500

    added parking lot dto in api project

---
+++ ParkAssist.API/Models/DTOs/ParkingLotDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ParkAssist.API.Models.DTOs
+{
+    public class ParkingLotDTO
+    {
+        public required int ParkingLotId { get; init; }
+        public required int OwnerId { get; init; }
+        public required string Name { get; init; }
+        public required string Address { get; init; }
+        public required string City { get; init; }
+        public required string State { get; init; }
+        public required string Zip { get; init; }
+    }
+}
