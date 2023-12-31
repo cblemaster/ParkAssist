105bd700167a4dda8228e01589bc19db30209905
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-30 19:59:44 -0500

    added jwt authentication middleware to program.cs in api project

--- ParkAssist.API/Program.cs
+++ ParkAssist.API/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MoneyTransfer.Security;
 using ParkAssist.API.Context;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +14,41 @@ IConfigurationRoot config = new ConfigurationBuilder()
             .Build();
 
 string connectionString = config.GetConnectionString("Project") ?? "Error retrieving connection string!";
+string jwtSecret = config.GetValue<string>("JwtSecret") ?? "Error retreiving jwt config!";
+
+var key = Encoding.ASCII.GetBytes(jwtSecret);
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.Sub] = "sub";
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        NameClaimType = "name"
+    };
+});
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("requireauthuser", policy => policy.RequireAuthenticatedUser());
 
 builder.Services.AddDbContext<ParkAssistContext>(options =>
     options.UseSqlServer(connectionString));
 
-var app = builder.Build();
+builder.Services
+    .AddSingleton<ITokenGenerator>(tk => new JwtGenerator(jwtSecret))
+    .AddSingleton<IPasswordHasher>(ph => new PasswordHasher());
+
+WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
