937bd318039f4794a840cead0fc22095d5891402
Brian LeMaster <brian.lemaster@outlook.com>
2024-01-02 19:21:11 -0500

    moved db connection string into config, registered db context in program.cs, and added authentication middleware to program.cs in api project

--- ParkAssist.API/Context/ParkAssistContext.cs
+++ ParkAssist.API/Context/ParkAssistContext.cs
@@ -35,8 +35,8 @@ public partial class ParkAssistContext : DbContext
     public virtual DbSet<Vehicle> Vehicles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=ParkAssist;Trusted_Connection=true;Trust Server Certificate=true");
+    {
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- ParkAssist.API/Program.cs
+++ ParkAssist.API/Program.cs
@@ -1,6 +1,63 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MoneyTransfer.Security;
+using ParkAssist.API.Context;
+using ParkAssist.API.Models.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+            .Build();
+
+string connectionString = config.GetConnectionString("Project") ?? "Error retrieving connection string!";
+string jwtSecret = config.GetValue<string>("JwtSecret") ?? "Error retreiving jwt config!";
+
+byte[] key = Encoding.ASCII.GetBytes(jwtSecret);
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.Sub] = "sub";
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        NameClaimType = "name"
+    };
+});
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("requireauthuser", policy => policy.RequireAuthenticatedUser());
+
+builder.Services.AddDbContext<ParkAssistContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services
+    .AddSingleton<ITokenGenerator>(tk => new JwtGenerator(jwtSecret))
+    .AddSingleton<IPasswordHasher>(ph => new PasswordHasher());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/Customer/{id:int}", async (int id, ParkAssistContext context) =>
+{
+    return (await context.Customers.SingleOrDefaultAsync(customer => customer.CustomerId == id))!;
+    
+});
 
 app.Run();
