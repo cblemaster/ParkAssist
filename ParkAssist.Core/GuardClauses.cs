ec6e64c0cb6e893b18ba31cd4d286d756e858a1c
Brian LeMaster <brian.lemaster@outlook.com>
2024-01-02 19:21:38 -0500

    added a guard clause in core project

--- ParkAssist.Core/GuardClauses.cs
+++ ParkAssist.Core/GuardClauses.cs
@@ -3,6 +3,7 @@
     public static class GuardClauses
     {
         public static bool IsNotNull<T>(T obj) where T : class => obj != null;
+        public static bool IsNull<T>(T obj) where T : class => obj == null;
         public static bool StringContainsChars(string str) => !string.IsNullOrWhiteSpace(str);
         public static bool StringLengthInValidRangeInclusive(int min, int max, string str) => str.Length >= min && str.Length <= max;
         public static bool StringLengthInValidRangeExclusive(int min, int max, string str) => str.Length > min && str.Length < max;
